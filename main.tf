efc6df1e6eac06d4b5af46d826c58f259fd44970
kurose-etsuo <kurose-etsuo@dmm.com>
2021-01-15 11:34:13 +0900

    test

--- main.tf
+++ main.tf
@@ -38,3 +38,13 @@ resource "aws_subnet" "test_subnet" {
     Name = "test-a"
   }
 }
+
+resource "aws_subnet" "test_subnet" {
+  vpc_id            = aws_vpc.test.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-northeast-1c"
+
+  tags = {
+    Name = "test-c"
+  }
+}
